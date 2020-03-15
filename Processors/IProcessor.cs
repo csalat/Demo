889a67227ab0ea1973829f4c46da38066b239dac
Csaba Salat <csalat@globalblue.com>
2020-03-15 12:51:08 +0100

    init

---
+++ Processors/IProcessor.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo.Processors
+{
+    public interface IProcessor<T>
+    {
+        Task<T> ExecuteAsync(T payload, CancellationToken token);
+    }
+}
---
+++ Processors/NullProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo.Processors
+{
+    public class NullProcessor<T> : IProcessor<T>
+    {
+        public async Task<T> ExecuteAsync(T payload, CancellationToken token)
+        {
+            return await Task.FromResult(payload).ConfigureAwait(false);
+        }
+    }
+}
